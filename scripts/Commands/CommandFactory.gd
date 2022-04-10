203f1cbe6a781062ed1821edacf162dd7dcbd323
Vincent Schmid <Vincent.Schmid@ti8m.ch>
2022-04-10 14:02:11 +0200

    formatting

--- scripts/Commands/CommandFactory.gd
+++ scripts/Commands/CommandFactory.gd
@@ -16,31 +16,20 @@ func _init(rng_gen: RngGen, map: PlayBoard, factory: PieceFactory):
 	_piece_factory = factory
 
 func create_add_random_piece_command() -> AddPieceCommand:
-	return AddPieceCommand.new(
-			_rng_gen,
-			_piece_factory,
-			_map,
-			_piece_factory,
-			Vector2(),
-			0)
+	return AddPieceCommand.new(_rng_gen, _piece_factory, _map, _piece_factory, Vector2(), 0)
 
 func create_add_piece_command(position: Vector2, value: int) -> AddPieceCommand:
-	return AddPieceCommand.new(
-			null,
-			_piece_factory,
-			_map,
-			_piece_factory,
-			position,
-			value)
+	return AddPieceCommand.new(null, _piece_factory, _map, _piece_factory, position, value)
 
-func create_move_command(board_position:  Vector2, next_board_position: Vector2, piece: Piece) -> MoveCommand:
-	return MoveCommand.new(
-		board_position,
-		next_board_position,
-		piece,
-		_map)
+func create_move_command(board_position: Vector2,
+		next_board_position: Vector2,
+		piece: Piece) -> MoveCommand:
+	return MoveCommand.new(board_position, next_board_position, piece, _map)
 
-func create_merge_command(board_position: Vector2, next_board_position: Vector2, piece: Piece, turnCommand: TurnCommand) -> MergeCommand:
+func create_merge_command(board_position: Vector2,
+		next_board_position: Vector2,
+		piece: Piece,
+		turnCommand: TurnCommand) -> MergeCommand:
 	return MergeCommand.new(board_position, 
 		next_board_position, 
 		piece, 
