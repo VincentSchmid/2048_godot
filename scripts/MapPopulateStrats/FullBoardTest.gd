7843c27855bdb34f6fc5852493be181a6517dbac
Vincent <puterich@gmail.com>
2023-10-09 12:29:05 +0200

    dynamic tile sizing

--- scripts/MapPopulateStrats/FullBoardTest.gd
+++ scripts/MapPopulateStrats/FullBoardTest.gd
@@ -4,7 +4,7 @@ extends MapPopulateStrategy
 class_name FullBoardTest
 
 func populate_map(map_size: int, piece_count: int, starting_pieces: Array) -> Array:
-	var map = .get_empty_map(map_size)
+	var map = get_empty_map(map_size)
 	var num = 0
 	for y in map.size():
 		for x in map.size()-1:
--- scripts/MapPopulateStrats/MergeTest.gd
+++ scripts/MapPopulateStrats/MergeTest.gd
@@ -4,7 +4,7 @@ extends MapPopulateStrategy
 class_name MergeTest
 
 func populate_map(map_size: int, piece_count: int, starting_pieces: Array) -> Array:
-	var map = .get_empty_map(map_size)
+	var map = get_empty_map(map_size)
 	map[1][0] = 2
 	map[1][1] = 2
 	map[1][2] = 4
