07a47a60c14a3d82ae0969c4a520c143eb2f51ca
Vincent <puterich@gmail.com>
2023-10-09 12:29:05 +0200

    dynamic tile sizing

--- scripts/MapPopulateStrats/MoveTest.gd
+++ scripts/MapPopulateStrats/MoveTest.gd
@@ -4,7 +4,7 @@ extends MapPopulateStrategy
 class_name MoveTest
 
 func populate_map(map_size: int, piece_count: int, starting_pieces: Array) -> Array:
-	var map = .get_empty_map(map_size)
+	var map = get_empty_map(map_size)
 	map[0][1] = 2
 
 	return map
